560bd7da914fc053db4d61525fab00e9bf4c4a1f
Maximiliano Kazanski <maxikazanski@gmail.com>
2025-11-02 17:21:55 -0300

    feat: add gaussian blur

--- src/preprocess.py
+++ src/preprocess.py
@@ -11,18 +11,18 @@ def image_to_vector(path, size=(63, 63), visualizar=True):
         raise ValueError(f"No se pudo leer la imagen: {path}")
 
     img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
-
-    img_resized = cv2.resize(img_gray, size)
-
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
+    img_resized = cv2.resize(img_blur, size)
     _, img_bin = cv2.threshold(img_resized, 127, 255, cv2.THRESH_BINARY)
 
     if visualizar:
         cv2.imshow("1 - Original", img_color)
         cv2.imshow("2 - Escala de grises", img_gray)
-        cv2.imshow(f"3 - Redimensionada {size}", img_resized)
-        cv2.imshow("4 - Binarizada (0/1)", img_bin)
+        cv2.imshow("3 - Gaussian blur", img_blur)
+        cv2.imshow(f"4 - Redimensionada {size}", img_resized)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+    # Normalizar a 0-1
     img_bin = (img_bin / 255).astype(np.uint8)
     return img_bin.flatten()
